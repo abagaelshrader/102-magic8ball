25cd12a32cd4d7418493aa44265f2935c4ed6624
abagael_S <abagael.shrader@gmail.com>
2020-06-29 11:00:15 -0500

    Commit 2 potential issues

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $ (document).ready(function(){
 
 	var magic8Ball = {};
   /*- array of the potential list of answers -*/
-	magic8Ball.magic8ballAnswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes- definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes."", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+	magic8Ball.magic8ballAnswers = ["It is certin.", "It is decidedly so.", "Without a doubt.", "Yes- definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes., Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 	$("#answer").hide(); /*hide the answer when the page finishes loading*/
 
   /* ----- askQuestion function ----- */
@@ -12,11 +12,10 @@ $ (document).ready(function(){
 		var randomNumberArray = randomNumber * this.magic8ballAnswers.length; /*creates a new variable that chooses a random number based on the length of the array - multiples the random number by the length of the array */
 		var randomIndex = Math.floor (randomNumberArray); /*round the number down - because we start counting at 0 */
 		var answer = this.magic8ballAnswers[randomIndex]; /*use the random number to index a random answer */
-    $("#answer").text(answer); /*connects random answer from array to text displayed in 8ball*/
+    $("#answer").text(answer);
 		console.log(question);
 		console.log(answer);
 	};
-  /*----- onClick function -----*/
 	var onClick = function() {
 		var question = prompt("Ask a YES or NO question please.");
 			magic8Ball.askQuestion(question);
@@ -24,6 +23,15 @@ $ (document).ready(function(){
       $("#8ball").effect("shake");
       $("#answer").fadeIn(4000);
 			$("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
+
+
+/*//wait half a second before showing prompt
+   setTimeout(
+       function() {
+           //show prompt
+           var question = prompt("Ask a yes or no question");
+           magic8Ball.askQuestion(question);
+       }, 500);*/
 
 };
 	$("#questionButton").click(onClick);
